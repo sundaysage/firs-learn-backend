51e49ace94130e2019198b31f725e9c479ee6405
sundaysage <semenike60@gmail.com>
2023-12-27 10:15:54 -0800

    funtion4

--- function/task4.js
+++ function/task4.js
@@ -2,6 +2,5 @@ function sumAll(...args) {
     return args.reduce((acc, curr) => acc + curr, 0);
 }
 
-// Example usage:
-const result = sumAll(1, 2, 3, 4, 5); // You can pass any number of integers
+const result = sumAll(1, 2, 3, 4, 5); 
 console.log(`The sum of all integers is: ${result}`);
